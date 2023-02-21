ce8dd380b5dcc42f0b788259929b499667d496f1
Kamil Kiliasiński <kamilkiliasinski@gmail.com>
2023-02-21 11:11:46 +0100

    change blog fallback

--- src/pages/blog/[slug]/index.tsx
+++ src/pages/blog/[slug]/index.tsx
@@ -7,7 +7,6 @@ import { PortableText } from '@portabletext/react';
 import { motion } from 'framer-motion';
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import { useNextSanityImage } from 'next-sanity-image';
-import { router } from 'next/client';
 import Img from 'next/image';
 
 interface Props {
@@ -19,7 +18,6 @@ export const getStaticProps: GetStaticProps<Props, { slug: string }> = async (
 ) => {
   const { slug } = ctx.params!;
   const post = await client.fetch(postBySlugQuery, { slug });
-
   return {
     props: {
       post,
@@ -32,7 +30,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
   const paths = await client.fetch(postByIdQuery);
   return {
     paths,
-    fallback: true,
+    fallback: false,
   };
 };
 
@@ -41,39 +39,35 @@ const Index: NextPage<Props> = ({ post }) => {
   const mainImage = gallery.images.find((image) => image.mainImage)!;
   const imageProps = useNextSanityImage(client, mainImage);
 
-  if (router.isFallback) {
-    return <div>Loading...</div>;
-  }
-
   return (
-      <motion.article
-          className="container mx-auto h-fit"
-          exit={{ opacity: 0 }}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.6 }}
-      >
-        <h1 className="mt-20 text-center text-4xl leading-snug md:my-20 md:text-6xl xl:text-9xl xl:leading-poppins-fit">
-          {post.title}
-        </h1>
-        {post.gallery.images.length === 1 ? (
-            <div className="mx-auto max-w-5xl">
-              <Img
-                  {...imageProps}
-                  src={urlFor(mainImage.asset).url()}
-                  alt={mainImage.asset.altText || title}
-                  priority
-                  placeholder="blur"
-                  blurDataURL={mainImage.asset.metadata.lqip}
-              />
-            </div>
-        ) : (
-            <Gallery postImages={post.gallery.images}/>
-        )}
-        <div className="mx-auto mb-24 w-10/12">
-          <PortableText value={body} components={components}/>
+    <motion.article
+      className="container mx-auto h-fit"
+      exit={{ opacity: 0 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.6 }}
+    >
+      <h1 className="mt-20 text-center text-4xl leading-snug md:my-20 md:text-6xl xl:text-9xl xl:leading-poppins-fit">
+        {post.title}
+      </h1>
+      {post.gallery.images.length === 1 ? (
+        <div className="mx-auto max-w-5xl">
+          <Img
+            {...imageProps}
+            src={urlFor(mainImage.asset).url()}
+            alt={mainImage.asset.altText || title}
+            priority
+            placeholder="blur"
+            blurDataURL={mainImage.asset.metadata.lqip}
+          />
         </div>
-      </motion.article>
+      ) : (
+        <Gallery postImages={post.gallery.images} />
+      )}
+      <div className="mx-auto mb-24 w-10/12">
+        <PortableText value={body} components={components} />
+      </div>
+    </motion.article>
   );
 };
 
